82d22b13f5d63d672696f0dd5594450d0612ecf1
bear-zyy <13520592875@163.com>
2019-01-09 11:11:20 +0800

    Initial Commit

---
+++ FileShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FileShow
+//
+//  Created by zyy on 2019/1/9.
+//  Copyright © 2019年 zyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
